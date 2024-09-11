6c2bc52ab78f171df5bc1c67d3795c9253abf2c1
entran02 <evanntran2002@gmail.com>
2024-09-11 17:22:18 -0400

    clean main

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,11 @@ mod cell_wrapper;
 
 #[tokio::main]
 async fn main() -> Result<(), fantoccini::error::CmdError> {
-
-    // Connect to the WebDriver.
     let client = ClientBuilder::native().connect("http://localhost:9515").await.expect("failed to connect to webdriver.");
 
     let mut board = Board::new(true, true, client).await?;
 
     board.play().await;
-
-    // Navigate to Google Minesweeper.
-    //client.goto("https://minesweeperonline.com/").await?;
-    //let url = client.current_url().await?;
-    //assert_eq!(url.as_ref(), "https://minesweeperonline.com/");
-
-    //client.wait().for_element(Locator::Css(".square.blank")).await?;
-
-    //let element = client.find(Locator::Css(r#"#\31_1"#)).await?;
-    //println!("{:?}", element);
-    
-    //client.find(Locator::Css(r#"#\31_1"#)).await?.click().await?;
-
-    // Here you would add more logic to interact with the game.
-
-    // Close the browser.
-    //client.close().await?;
 
     Ok(())
 }
